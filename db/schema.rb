7aa6f55df3b160cd1c33457f37f97c5c6d673dae
dAhuja <dahuja@plumslice.com>
2016-07-05 10:27:36 +0530

    model included

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160704051704) do
+ActiveRecord::Schema.define(version: 20160705045233) do
+
+  create_table "abcs", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "plums", force: true do |t|
     t.string   "name"
